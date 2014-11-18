da5b6d9c0a50fa9f2e4707deccd18cefb1235271
Patrick Herbert <patricktrherbert@gmail.com>
2014-11-18 14:18:47 -0800

    Added Ball.h & Screen.h
    Now there are 3 classes:
    Dog, Ball, Screen

---
+++ FinalProject/FinalProject/Ball.h
@@ -0,0 +1,19 @@
+#ifndef _BALL_
+#define _BALL_
+
+#include <iostream>
+using namespace std;
+
+class Ball
+{
+private:
+	int xpos;
+	int ypos;
+
+public:
+	int getXPos();
+	int getYPos();
+
+};
+
+#endif _BALL_
---
+++ FinalProject/FinalProject/Screen.h
@@ -0,0 +1,15 @@
+#ifndef _SCREEN_
+#define _SCREEN_
+
+#include <iostream>
+using namespace std;
+
+class Screen
+{
+private:
+
+public:
+	
+};
+
+#endif _SCREEN_
