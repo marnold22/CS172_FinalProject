a67e03ad61d2899c5070146e4fe4967a2bd31225
Patrick Herbert <patricktrherbert@gmail.com>
2014-11-18 14:13:34 -0800

    Setting Up Dog Header File
    Dog.h

---
+++ FinalProject/FinalProject/Dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_
+#define _DOG_
+
+#include <iostream>
+using namespace std;
+
+class Dog
+{
+private:
+	int xpos;
+	int ypos;
+
+public:
+	int getXPos();
+	int getYPos();
+
+};
+
+#endif _DOG_
